1ac3154690b66b5f9798f13437fd572e40761df6
DwipshikhaLodh <lodhdwipshikha@gmail.com>
2025-09-11 22:28:45 +0530

    added features, layout, shared & core folders

---
+++ src/app/layout/main/main-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ed-mainlayout-wrapper',
+    templateUrl: 'main-layout.component.html'
+})
+
+export class MainLayout{
+    
+}
